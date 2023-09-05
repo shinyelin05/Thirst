d8cde15060e4a8463476322c77b303650d1ac37e
yelin <shin0411a@naver.com>
2023-09-06 02:02:10 +0900

    적 만드는 중

--- Assets/01.Scripts/Enemy/EnemyController.cs
+++ Assets/01.Scripts/Enemy/EnemyController.cs
@@ -9,10 +9,10 @@ public class EnemyController : MonoBehaviour
     EnemyState curState;
     float stateTimer;
 
-    Vector2 idleMoveDir;
+    Vector3 idleMoveDir;
     public PlayerController player;
-
-    float enemySpeed = 3f;
+    public Animator animator;
+    float enemySpeed = 2f;
     float dist;
 
     enum EnemyState
@@ -30,10 +30,13 @@ public class EnemyController : MonoBehaviour
         curState = state;
     }
 
+    private void Awake()
+    {
+        Animator animator = GetComponent<Animator>();
+    }
+
     private void Update()
     {
-         
-
         stateTimer += Time.deltaTime;
         switch (curState)
         {
@@ -57,31 +60,51 @@ public class EnemyController : MonoBehaviour
 
     void IdleState()
     {
+        enemySpeed = 2f;
+
+        animator.SetBool("IsTrance", true);
+
         dist = Vector3.Distance(transform.position, player.gameObject.transform.position);
 
         if (stateTimer > 0)
         {
-            stateTimer = -Random.Range(2.5f, 5f);
-            idleMoveDir = Random.insideUnitCircle.normalized;
+            stateTimer = -Random.Range(5f, 8f);
+            idleMoveDir = Random.insideUnitSphere.normalized;
+
+            idleMoveDir.y = 0;
         }
 
         transform.position += (Vector3)idleMoveDir * Time.deltaTime * enemySpeed;
 
         if (dist < 20)
         {
+            animator.SetBool("IsTrance", false);
             ChangeState(EnemyState.Chase);
         }
     }
 
     void ChaseState()
     {
+        enemySpeed = 4f;
+
+        animator.SetBool("IsRun", true);
+
         float dist = Vector3.Distance(transform.position, player.gameObject.transform.position);
         Vector3 dir = (player.gameObject.transform.position - transform.position).normalized;
+        
+        dir.y = 0;
+        
         transform.position += dir * enemySpeed * Time.deltaTime;
+
+        Vector3 dir2 = player.transform.position - this.transform.position;
+
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.LookRotation(dir2), Time.deltaTime * enemySpeed);
+
 
         // 만약에 거리가 5보다 적으면 Idle로 하기.
         if (dist > 20)
         {
+            animator.SetBool("IsRun", false);
             ChangeState(EnemyState.Idle);
         }
     }
