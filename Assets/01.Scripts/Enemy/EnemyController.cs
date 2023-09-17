f454b1f78912b8034ec5cc17005915d43cc4b889
yelin <shin0411a@naver.com>
2023-09-17 23:59:29 +0900

    tnwjd

--- Assets/01.Scripts/Enemy/EnemyController.cs
+++ Assets/01.Scripts/Enemy/EnemyController.cs
@@ -6,7 +6,9 @@ public class EnemyController : Entity
 {
     [Header("적 스테이트 관련")]
     EnemyState curState;
+    EnemyState prevState = EnemyState.Die;
     float stateTimer;
+    bool isStateFirst;
     bool isDie;
 
     [Header("적 이동 관련")]
@@ -41,7 +43,7 @@ public class EnemyController : Entity
 
     void ChangeState(EnemyState state)
     {
-        Debug.Log("Change :" + state);
+        Debug.Log("Change :" + state + gameObject.GetInstanceID());
         enemySpeed = 2f;
         stateTimer = 0;
         curState = state;
@@ -57,34 +59,29 @@ public class EnemyController : Entity
 
     private void Update()
     {
-
-        if (Input.GetKey(KeyCode.Escape))
+        if(curState != prevState)
         {
-            Debug.DrawRay(transform.position, transform.forward * maxDistance, Color.blue, 0.3f);
-            if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance))
+            isStateFirst = true;
+            prevState = curState;
+        }
+
+        Debug.DrawRay(transform.position, transform.forward * maxDistance, Color.blue, 0.3f);
+        if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance))
+        {
+            if (hit.collider == null)
             {
-                if (hit.collider == null)
-                {
-                    return;
-                }
-                if (hit.collider.CompareTag("STONE"))
-                {
-                    Debug.Log(hit.collider.name + "돌!!!!!!!!!!!!!!!!!!!!!!!");
-
-                }
+                return;
             }
+            if (hit.collider.CompareTag("STONE"))
+            {
+                Debug.Log(hit.collider.name + "돌!!!!!!!!!!!!!!!!!!!!!!!");
 
+            }
         }
 
         if (enemyHP.Hp <= 0)
         {
-
             curState = EnemyState.Die;
-        }
-
-        if (isDamaged)
-        {
-            ChangeState(EnemyState.Hit);
         }
 
         stateTimer += Time.deltaTime;
@@ -114,10 +111,14 @@ public class EnemyController : Entity
                 DieState();
                 break;
         }
+        isStateFirst = false;
     }
 
     void IdleState()
     {
+        enemySpeed = 0;
+        animator.SetFloat("Speed", 0);
+
         if (stateTimer > 1)
         {
             ChangeState(EnemyState.Trance);
@@ -126,7 +127,7 @@ public class EnemyController : Entity
 
     void TranceState()
     {
-        animator.SetBool("IsTrance", true);
+        animator.SetFloat("Speed", 1);
 
         enemyDist = Vector3.Distance(transform.position, player.gameObject.transform.position);
 
@@ -144,16 +145,15 @@ public class EnemyController : Entity
 
         if (enemyDist < 10)
         {
-            animator.SetBool("IsTrance", false);
             ChangeState(EnemyState.Chase);
         }
     }
 
     void ChaseState()
     {
-        enemySpeed = 4f;
+        animator.SetFloat("Speed", 2);
 
-        animator.SetBool("IsRun", true);
+        enemySpeed = 4f;
 
         float dist = Vector3.Distance(transform.position, player.gameObject.transform.position);
         Vector3 dir = (player.gameObject.transform.position - transform.position).normalized;
@@ -166,35 +166,36 @@ public class EnemyController : Entity
 
         if (dist >= 10)
         {
-            animator.SetBool("IsRun", false);
             ChangeState(EnemyState.Trance);
         }
 
-        else if (dist < 1)
+        if (dist < 1)
         {
-            animator.SetBool("IsRun", false);
             ChangeState(EnemyState.Attack);
         }
     }
 
     void AttackState()
     {
-        animator.SetBool("IsAttack", true);
-        player.PlayerDamage(0.1f);
-        player.isDamage = true;
+        Debug.Log("Attack" + gameObject.GetInstanceID());
+        if (isStateFirst)
+        {
+            Debug.Log("Atk" + gameObject.GetInstanceID());
+            animator.SetTrigger("Attack");
+            player.PlayerDamage(10f);
+            player.isDamage = true;
+        }
 
         if (stateTimer >= 2f)
         {
             ChangeState(EnemyState.Idle);
-            animator.SetBool("IsAttack", false);
             player.isDamage = false;
         }
     }
 
     void DieState()
     {
-        animator.SetBool("IsRun", false);
-        animator.SetBool("IsTrance", false);
+        animator.SetFloat("Speed", 0);
 
         Destroy(enemyHPSlider);
 
@@ -207,14 +208,19 @@ public class EnemyController : Entity
 
     void HitState()
     {
+        Debug.Log("Hit" + gameObject.GetInstanceID());
+        if (isStateFirst)
+        {
+            Debug.Log("Hittt" + gameObject.GetInstanceID());
+            animator.SetTrigger("Hit");
+        }
 
-        animator.SetBool("IsRun", false);
-        animator.SetBool("IsGetHit", true);
+        enemySpeed = 0;
+        animator.SetFloat("Speed", 0);
 
         if (stateTimer > 1)
         {
             ChangeState(EnemyState.Idle);
-            animator.SetBool("IsGetHit", false);
         }
     }
 
@@ -231,6 +237,5 @@ public class EnemyController : Entity
     public void PlayerInit(PlayerController owner)
     {
         player = owner;
-
     }
 }
